3fb65b66490fc2f77585cbf79b2195fa03783424
Alex <alexbss1996@gmail.com>
2022-07-10 19:21:42 +0200

    ranking quasi ok

--- Angular/src/app/@core/services/ranking.service.ts
+++ Angular/src/app/@core/services/ranking.service.ts
@@ -12,12 +12,20 @@ export class RankingService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getRanking(userId: number){
+  getRankingByUserId(userId: number){
     return this.httpClient.get<Ranking>(`${this.nodeBaseUrl}/ranking/${userId}`);
   }
 
   createRanking(ranking: Partial<Ranking>){
     return this.httpClient.post<Ranking>(`${this.nodeBaseUrl}/ranking`, ranking); 
+  }
+
+  updateRanking(userId: number | undefined, gamePoints: number | undefined){
+    return this.httpClient.patch<Ranking>(`${this.nodeBaseUrl}/ranking/${userId}`, gamePoints);
+  }
+
+  getRanking(){
+    return this.httpClient.get<Ranking[]>(`${this.nodeBaseUrl}/ranking`);
   }
 }
 
