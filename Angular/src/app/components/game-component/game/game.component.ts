1870c02e6f68db41efdee3e173c6d70516f55d90
Alex <alexbss1996@gmail.com>
2022-07-23 22:01:03 +0200

    fix button drag e drop

--- Angular/src/app/components/game-component/game/game.component.ts
+++ Angular/src/app/components/game-component/game/game.component.ts
@@ -16,11 +16,11 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 export class GameComponent implements OnInit {
 
   constructor(private movieService: MovieService, private rankingService: RankingService, private authService: AuthService) { }
-  
+
   movies = this.movieService.movies;
   movie: Partial<Movie> = {};
   orderedMovies: Partial<Movie>[] = [];
-  criterion : Criterion = {label: "", key: ''};
+  criterion: Criterion = { label: "", key: '' };
   currentUserId = this.authService.getCurrentUser().id;
   imageBaseUrl: string = "https://image.tmdb.org/t/p/w440_and_h660_face"
   game = false;
@@ -28,60 +28,56 @@ export class GameComponent implements OnInit {
 
 
   ngOnInit(): void {
-    for(let index = 0; index < 10; index++){
+    for (let index = 0; index < 10; index++) {
       this.movieService.getRandomMovie(index);
     }
     this.criterion = this.movieService.getRandomCriterion();
     console.log(this.criterion.key);
-    }
+  }
 
-    drop(event: CdkDragDrop<{title: string; poster: string}[]>) {
-      moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
-      console.log(this.movies);
-    }
+  drop(event: CdkDragDrop<{ title: string; poster: string }[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+    console.log(this.movies);
+  }
 
-    showGame(){
+  showGame() {
     this.game = !this.game;
   }
 
-  showResults(){
-    this.results = !this.results;
-  }
 
-
-  startGame(){
-    for(let i = 0; i < 10; i++){
+  startGame() {
+    for (let i = 0; i < 10; i++) {
       this.orderedMovies.push(this.movieService.movies[i]);
     }
 
-    for(let i = 0; i < 10; i++){
+    for (let i = 0; i < 10; i++) {
       console.log(this.orderedMovies[i]);
     }
-    
+
     this.descendingOrder(this.criterion.key)
-    
-    for(let i = 0; i < 10; i++){
+
+    for (let i = 0; i < 10; i++) {
       console.log(this.orderedMovies[i]);
     }
   }
 
-  finish(form: NgForm){
+  finish(form: NgForm) {
     console.log(this.orderedMovies);
     console.log(this.movies);
     let points: number = 0;
-    for(let i = 0; i < 10; i++){
-      if(this.orderedMovies[i] == this.movies[i]){
+    for (let i = 0; i < 10; i++) {
+      if (this.orderedMovies[i] == this.movies[i]) {
         points = points + 10;
-        }
+      }
     }
-      this.rankingService.createRanking({userId: this.currentUserId, gamePoints: points}).subscribe({
-        next: (res) => {
+    this.rankingService.createRanking({ userId: this.currentUserId, gamePoints: points }).subscribe({
+      next: (res) => {
         console.log(res);
       }
     });
   }
 
-  descendingOrder(criterion: string) {    
+  descendingOrder(criterion: string) {
     this.orderedMovies?.sort((a: any, b: any) => {
       if (a[criterion] != undefined && b[criterion] != undefined) {
         if (b[criterion] > a[criterion]) {
